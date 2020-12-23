549812c4a22a259b4fda7311d6abdca6fc6d55e7
zxd123 <1173474141@qq.com>
2020-12-23 16:31:16 +0800

    communication_middleware 框架基本完成

--- communication_middleware/src/ndt_to_viewmsg.cpp
+++ communication_middleware/src/ndt_to_viewmsg.cpp
@@ -1,3 +1,9 @@
 //
 // Created by zxd on 2020/12/23.
 //
+#include "communication_middleware/ndt_to_viewmsg.h"
+namespace communication_middleware {
+ void NdtToViewmsg::VoxelGridCovarianceToViewmsg(const Eigen::Matrix3d& cov, const Eigen::Vector3d& mean, const ndt_view_msgs::NDTViewArray& msg) {
+
+ }
+}
