9aa8d79c1efe6f209bb5d06c6667a88bee414439
zxd123 <1173474141@qq.com>
2020-12-23 16:31:16 +0800

    communication_middleware 框架基本完成

--- communication_middleware/include/communication_middleware/ndt_to_viewmsg.h
+++ communication_middleware/include/communication_middleware/ndt_to_viewmsg.h
@@ -4,11 +4,19 @@
 
 #ifndef SRC_NDT_TO_VIEWMSG_H
 #define SRC_NDT_TO_VIEWMSG_H
-//#include <Eigen3/core>
-//
-//class NdtToViewmsg{
-// public:
-//  NdtToViewmsg(){};
-//  VoxelGridCovarianceToViewmsg(Eigen::);
-//};
+#include <Eigen/Core>
+#include "ndt_view_msgs/NDTView.h"
+#include "ndt_view_msgs/NDTViewArray.h"
+namespace communication_middleware {
+class NdtToViewmsg {
+ public:
+  NdtToViewmsg() {};
+
+//  将体素的均值方差信息转换为可显示的rosmsg
+//  参数1:输入，体素的方差
+//  参数2:输入，体素的均值
+//  参数3:输出，rosmsg
+  void VoxelGridCovarianceToViewmsg(const Eigen::Matrix3d& cov, const Eigen::Vector3d& mean, const ndt_view_msgs::NDTViewArray& msg);
+};
+}
 #endif //SRC_NDT_TO_VIEWMSG_H
