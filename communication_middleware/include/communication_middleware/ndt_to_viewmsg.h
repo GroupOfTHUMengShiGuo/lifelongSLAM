542b400cc68d1111c09fcaeb5d3c66df6e98eba5
zxd123 <1173474141@qq.com>
2020-12-23 15:06:43 +0800

    communication_middleware init

---
+++ communication_middleware/include/communication_middleware/ndt_to_viewmsg.h
@@ -0,0 +1,14 @@
+//
+// Created by zxd on 2020/12/23.
+//
+
+#ifndef SRC_NDT_TO_VIEWMSG_H
+#define SRC_NDT_TO_VIEWMSG_H
+//#include <Eigen3/core>
+//
+//class NdtToViewmsg{
+// public:
+//  NdtToViewmsg(){};
+//  VoxelGridCovarianceToViewmsg(Eigen::);
+//};
+#endif //SRC_NDT_TO_VIEWMSG_H
